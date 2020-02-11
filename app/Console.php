be7b8b3025989ac9d4f90f1aad7d90505a4a5d2b
Hatchet Pty Ltd <accounts@hatchet.com.au>
2020-02-11 12:12:45 +0800

    Update Console.php
    Change AWS IAM URL to a prettier URL based on AWS docs with an example Account ID

--- app/Console.php
+++ app/Console.php
@@ -18,7 +18,7 @@ class Console extends Model
         ],
         [
             'name' => 'AWS Console w/IAM Account',
-            'url' => 'https://signin.aws.amazon.com/oauth?redirect_uri=https%3A%2F%2Fconsole.aws.amazon.com%2Fconsole%2Fhome%3Fstate%3DhashArgs%2523%26isauthcode%3Dtrue&client_id=arn%3Aaws%3Aiam%3A%3A015428540659%3Auser%2Fhomepage&response_type=code&iam_user=true&account=',
+            'url' => 'https://123456789.signin.aws.amazon.com/console/',
             'provider' => 'Amazon',
             'route' => 'amazon/with-account',
         ],
