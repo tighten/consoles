c8aaaa689714e32713d930365d87ff2e809167bd
Matt Stauffer <matt@tighten.co>
2020-02-11 10:50:15 -0500

    Add subdomain tip

--- resources/views/consoles/index.blade.php
+++ resources/views/consoles/index.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="container max-w-2xl mx-auto">
         <div class="md:flex">
-            <div class="flex-none mb-10 md:mb-6 md:mr-8 mx-auto w-48 md:w-64">
-                <img src="/images/undraw_dashboard_nklg.svg" alt="Illustration of a man standing in front of a computer monitor showing a dashboard.">
+            <div class="flex-none mb-10 md:mb-6 md:mr-8 mx-auto w-64 md:w-64">
+                <img class="mx-auto w-48 md:w-64" src="/images/undraw_dashboard_nklg.svg" alt="Illustration of a man standing in front of a computer monitor showing a dashboard.">
+                <div class="hidden md:block mt-6">@include('partials.subdomain-tip')</div>
             </div>
             <div class="flex-grow rounded mx-2 md:mx-0">
                 <div class="w-full p-2 md:p-6 text-xl">
@@ -12,11 +13,14 @@
                         <div class="mb-4">
                             <div class="font-semibold mb-2 text-gray-700 text-lg uppercase">{{ $provider->name }}</div>
                             @foreach ($provider->consoles as $console)
-                                <a href="{{ $console->url }}" class="bg-blue-700 hover:bg-blue-800 inline-block border font-semibold mr-2 py-3 px-6 rounded-md text-blue-100 hover:text-white mb-4">{{ $console->name }}</a><br>
+                                <a href="{{ $console->route }}" class="bg-blue-700 hover:bg-blue-800 inline-block border font-semibold mr-2 py-3 px-6 rounded-md text-blue-100 hover:text-white mb-4">{{ $console->name }}</a><br>
                             @endforeach
                         </div>
                     @endforeach
                 </div>
+            </div>
+            <div class="md:hidden px-4 mb-8">
+                @include('partials.subdomain-tip')
             </div>
         </div>
     </div>
