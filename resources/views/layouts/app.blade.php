faf97d52124662535e7d2b78be968f0984874229
Matt Stauffer <matt@tighten.co>
2020-04-21 23:05:35 -0400

    Open Graph and Twitter meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="A list of API management consoles">
+
+    <meta property="og:site_name" content="Consoles.dev">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:title" content="Consoles.dev">
+    <meta property="og:url" content="https://consoles.dev/">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="A list of API management consoles">
+
+    <meta property="og:image" content="http://consoles.dev/images/opengraph.png">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:width" content="1200">
+
+    <meta name="twitter:site" content="@stauffermatt">
+    <meta name="twitter:creator" content="@stauffermatt">
+    <meta name="twitter:title" content="Consoles.dev">
+    <meta name="twitter:description" content="A list of API management consoles">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="http://consoles.dev/images/opengraph.png">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
