817289efcfddbc91a476e0b61f0435e59d9a9757
Matt Stauffer <matt@tighten.co>
2021-12-16 13:37:37 -0500

    Upgrade frontend dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,5 @@
 const mix = require('laravel-mix');
 
-require('laravel-mix-tailwind');
-require('laravel-mix-purgecss');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -15,11 +12,11 @@ require('laravel-mix-purgecss');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+   .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+   ])
 
 if (mix.inProduction()) {
   mix
-   .version()
-   .purgeCss();
+   .version();
 }
