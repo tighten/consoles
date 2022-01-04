9bf02c26b3023521b0dc2a42cfa625b272120384
Sonny Spaan <info@spaanproductions.nl>
2022-01-04 11:40:43 +0100

    Fix Tailwind v3.x Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
   theme: {
     fontFamily: {
       display: ['Special Elite', 'cursive'],
