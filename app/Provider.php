2c157b60ebc51e13d553495adfdff7cc489b07e2
Shift <shift@laravelshift.com>
2024-04-11 19:28:33 +0000

    Shift cleanup

--- app/Provider.php
+++ app/Provider.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Sushi\Sushi;
 
 class Provider extends Model
