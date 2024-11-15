2df3c9f7cd4e222b64d525a1e65b43b8d2eac1ea
Shift <shift@laravelshift.com>
2024-11-15 14:17:31 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
-
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
