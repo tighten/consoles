3131d9aca2d83c6f55254f7068b6630380f3fe81
Shift <shift@laravelshift.com>
2026-03-21 03:42:13 +0000

    Bump NPM dependencies

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
